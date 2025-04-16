a5258a6275b8d82af99fa47aa931829b6db972fe
Sharvin Soham <sharvinsoham@hotmail.com>
2025-04-17 02:45:34 +0530

    Redirected import from HomePage.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import HomePage from './HomePage';
+import HomePage from "./HomePage.jsx";
 import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
